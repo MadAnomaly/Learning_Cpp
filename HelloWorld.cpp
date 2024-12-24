e31c7b6257ff7bd72fdf31f489119f848095a171
MadAnomaly <42014840+MadAnomaly@users.noreply.github.com>
2024-12-24 12:29:43 +0400

    Added HelloWorld.cpp
    First commit!

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <locale>
+
+int main() {
+    std::setlocale(LC_ALL, "Russian"); // Устанавливаем русскую локаль
+    std::cout << "Visual Studio настроен для C++!" << std::endl;
+    return 0;
+}
